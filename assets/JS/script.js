17d9d1cfcf39069c316fd91988bf889e2ef60aa8
Geraldine Edwards <geraldine.edwards25@gmail.com>
2025-06-04 10:51:05 +0100

    Update players variable in script.js to include extra players

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,4 +1,4 @@
-const players = ["Player 1", "Player 2", "Player 3", "Player 4"]; 
+const players = ["Player 1", "Player 2", "Player 3", "Player 4", "Player 5", "Player 6"]; 
 const gameTypes = ["Easy", "Hard", "Matchplay"];
 const totalCourses = 33;
 
